57114a9335ad34cee7dc463e87f3d66836e67516
samjwalston <samjwalston@gmail.com>
2015-01-20 09:24:01 -0800

    Second Revision

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,17 +1,39 @@
 var wins = 0;
+var winStreak = 0;
 var losses = 0;
 var ties = 0;
 var computerChoice = "";
 var ucArray = [];
 var ucaLength = "";
+var names = {
+	rNames:  ["Robert", "Richard", "Raymond", "Randy", "Roger", "Ronald", "Russel", "Reese", "Ralph", "Romeo"],
+	nNames: ["Noel", "Noah", "Nathan", "Nicholas", "Neil", "Nelson", "Nico", "Nigel", "Norton", "Norman"],
+	gNames: ["Gaeth", "Garrett", "Goldburg", "Gundry", "Gates", "Gotham", "Grace", "Getty", "Glidden", "Ghent"], 
+};
 
+function total(){
+	var totalGames = "Total Games: " + (wins + losses + ties);
+	document.getElementById("total").innerHTML = totalGames;
+};
+function won(){
+	var sum = (wins + losses + ties);
+	var wonGames = "Wins: " + wins + " (" + ((wins / sum) * 100).toFixed(2) + "%)";
+	document.getElementById("won").innerHTML = wonGames;
+};
+function lost(){
+	var sum = (wins + losses + ties);
+	var lostGames = "Losses: " + losses + " (" + ((losses / sum) * 100).toFixed(2) + "%)";
+	document.getElementById("lost").innerHTML = lostGames;
+};
+function tied(){
+	var sum = (wins + losses + ties);
+	var tiedGames = "Ties: " + ties + " (" + ((ties / sum) * 100).toFixed(2) + "%)";
+	document.getElementById("tied").innerHTML = tiedGames;
+};
 function winner(result){
-		document.getElementById("winner").innerHTML = result;
+	document.getElementById("winner").innerHTML = result;
 };
-function score(){
-	var counter = wins + "/" + losses + "/" + ties;
-	document.getElementById("score").innerHTML = counter;
-};
+
 function rockPaperScissors(userChoice){
 	function patternRandom(){
 		computerChoice = Math.random();
@@ -35,7 +57,7 @@ function rockPaperScissors(userChoice){
 		else if(ucArray[ucaLength - 1] == "scissors"){
 			computerChoice = "rock";
 		};
-	}
+	};
 	function patternABAB(){
 		if(ucArray[ucaLength - 1] == ucArray[ucaLength - 3]){
 			if(ucArray[ucaLength - 2] == ucArray[ucaLength - 4]){
@@ -52,7 +74,7 @@ function rockPaperScissors(userChoice){
 		};
 	};
 	function patternABCABC(){
-		if((ucArray[ucaLength - 1] == ucArray[ucaLength - 4]) && (ucArray[ucaLength - 2] == ucArray[ucaLength - 5]) && (	ucArray[ucaLength - 3] == ucArray[ucaLength - 6])){
+		if((ucArray[ucaLength - 1] == ucArray[ucaLength - 4]) && (ucArray[ucaLength - 2] == ucArray[ucaLength - 5]) && (ucArray[ucaLength - 3] == ucArray[ucaLength - 6])){
 			if(ucArray[ucaLength - 3] == "rock"){
 				computerChoice = "paper";
 			}
@@ -67,68 +89,109 @@ function rockPaperScissors(userChoice){
 
 	const tieMessage = "It's a tie...";
 	const winMessage = "You win! :D";
-	const loseMessage = "Computer wins. D:";
+	const loseMessage = names.rNames[Math.floor(Math.random() * 9)] + " " + names.nNames[Math.floor(Math.random() * 9)] + " " + names.gNames[Math.floor(Math.random() * 9)]
+ + " wins. D:";
+ 	const greatMessage = "Damn son! You're on fire!";
+ 	const tantrumMessage = "Fuck this game!";
 
 	var ucaLength = ucArray.length;
 
 	if(ucArray.length == 0){
 		patternRandom();
 	}
-	else if(userChoice == ucArray[ucaLength - 1]){
+	else if(ucArray[ucaLength - 1] == ucArray[ucaLength - 2]){
 		patternAA();
 	}
-	else if(userChoice == ucArray[ucaLength - 2]){
+	else if(ucArray[ucaLength - 1] == ucArray[ucaLength - 3]){
 		patternABAB();
 	}
-	else if(userChoice == ucArray[ucaLength - 3]){
+	else if(ucArray[ucaLength - 1] == ucArray[ucaLength - 4]){
 		patternABCABC();
 	}
 	else{
 		patternRandom();
 	};
 
-	ucArray.push(userChoice);
-	ucaLength = ucArray.length;
-
 	if(userChoice == computerChoice){
-		winner(tieMessage);
 		++ties;
-    score();
+		winStreak = 0;
+		total();
+    won();
+    lost();
+    tied();
+    winner(tieMessage);
   }
   else if(userChoice == "rock"){
     if(computerChoice == "scissors"){
-      winner(winMessage);
       ++wins;
-      score();
+      ++winStreak;
+      total();
+      won();
+    	lost();
+    	tied();
+      winner(winMessage);
     }
     else{
-    	winner(loseMessage);
     	++losses;
-    	score();
+    	winStreak = 0;
+    	total();
+    	won();
+    	lost();
+    	tied();
+    	winner(loseMessage);
     };
   }
   else if(userChoice == "paper"){
     if(computerChoice == "rock"){
-      winner(winMessage);
       ++wins;
-      score();
+      ++winStreak;
+      total();
+      won();
+    	lost();
+    	tied();
+      winner(winMessage);
     }
     else{
-      winner(loseMessage);
       ++losses;
-      score();
+      winStreak = 0;
+      total();
+      won();
+    	lost();
+    	tied();
+      winner(loseMessage);
     };
   }
   else{
     if (computerChoice == "rock"){
-      winner(loseMessage);
       ++losses;
-      score();
+      winStreak = 0;
+      total();
+      won();
+    	lost();
+   		tied();
+      winner(loseMessage);
     }
     else{
-      winner(winMessage);
       ++wins;
-      score();
+      ++winStreak;
+      total();
+      won();
+    	lost();
+    	tied();
+      winner(winMessage);
     };
   };
+
+  ucArray.push(userChoice);
+	ucaLength = ucArray.length;
+	if(ucaLength > 10){
+		ucArray.shift();
+	};
+	if(winStreak >= 10){
+		winner(tantrumMessage);
+		throw new Error();
+	}
+	else if(winStreak >= 5){
+		winner(greatMessage);
+	};
 };
